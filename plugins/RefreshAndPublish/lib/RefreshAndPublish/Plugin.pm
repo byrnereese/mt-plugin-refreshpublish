41ac8525b34a5cadb6c283df42f7aaab2e7631fc
Byrne <byrne@majordojo.com>
2010-10-22 21:45:19 -0700

    Added mt/melody compatibility layer

--- plugins/RefreshAndPublish/lib/RefreshAndPublish/Plugin.pm
+++ plugins/RefreshAndPublish/lib/RefreshAndPublish/Plugin.pm
@@ -4,19 +4,21 @@ use strict;
 use Carp qw( croak );
 
 sub cond {
-    my $app = MT->app;                                                                                                             
-    my $tmpl_type = $app->param('filter_key');                                                                                     
-    return $app->mode eq 'itemset_action'  ? 1                                                                                     
-	: !$app->blog                     ? 0                                                                                     
-	: !$tmpl_type                     ? 0                                                                                     
-	: $tmpl_type eq 'index_templates' ? 1                                                                                     
-	:                                   0                                                                                     
-	;                                                                                                                         
+    my $app = MT->app;
+    my $q = $app->can('query') ? $app->query : $app->param;
+    my $tmpl_type = $q->param('filter_key');
+    return $app->mode eq 'itemset_action'  ? 1
+	: !$app->blog                     ? 0
+	: !$tmpl_type                     ? 0
+	: $tmpl_type eq 'index_templates' ? 1
+	:                                   0
+	;
 }                                                                                                                                 
 
 sub refpub {
     my $app = shift;
     $app->validate_magic or return;
+    my $q = $app->can('query') ? $app->query : $app->param;
 
     # permission check
     my $perms = $app->permissions;
@@ -29,7 +31,7 @@ sub refpub {
     MT::CMS::Template::refresh_individual_templates($app);
 
     my $blog = $app->blog;
-    my $templates = MT->model('template')->lookup_multi([ $app->param('id') ]);
+    my $templates = MT->model('template')->lookup_multi([ $q->param('id') ]);
     TEMPLATE: for my $tmpl (@$templates) {
         next TEMPLATE if !defined $tmpl;
         next TEMPLATE if $tmpl->blog_id != $blog->id;
